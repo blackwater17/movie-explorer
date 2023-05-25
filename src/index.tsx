8fe2d7f83c47b717de13d17d1ee1a7b9c5d300d6
blackwater17 <dextermorgan_7@hotmail.com>
2023-05-25 14:30:05 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import configureStore from './store/configureStore';
 
 const store = configureStore(); // just shows reducer paths
 
-console.log('Running.');
+console.log('Running....');
 
 const jsx = (
   <Provider store={store}>
